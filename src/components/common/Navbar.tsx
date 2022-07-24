5e6a01e8bb6deb6ef21a3be4a8effbdf45170ac8
Oz4 <aowss18@gmail.com>
2022-07-25 02:55:26 +0300

    added burger menue for small screens

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,14 +1,129 @@
-import React, { useContext } from "react"
-import { Box, Flex, Text } from "@chakra-ui/react"
-import { ConnectWallet, DisconnectWallet } from "components/common"
+import React, { useContext, useEffect, useState } from "react"
+import { Box, Flex, Stack, Text } from "@chakra-ui/react"
+import { ConnectWallet, DisconnectWallet, LRDrawer } from "components/common"
 import { LRLogo } from "assets/logos"
 import { WalletProviderContext, WalletProviderInterface } from "setup/WalletProvider"
 import { getAddressShortcut } from "utils"
 import Link from "next/link"
 
+const NormalNavbar = ({
+  walletAddress
+}: {
+  walletAddress: string | undefined
+}) => {
+
+  return (
+    <Flex align="center">
+      <Link href="/collections">
+        <Text
+          mr="2rem"
+          cursor="pointer"
+          fontSize={[
+            "var(--lr-font-size-10)",
+            "var(--lr-font-size-12)",
+            "var(--lr-font-size-14)",
+            "var(--lr-font-size-16)",
+          ]}
+        >
+          Collections
+        </Text>
+      </Link>
+      {walletAddress ?
+        <Flex align="center">
+          <Text
+            mr="1rem"
+            color="var(--lr-color-accent-green)"
+            fontSize={[
+              "var(--lr-font-size-10)",
+              "var(--lr-font-size-12)",
+              "var(--lr-font-size-14)",
+              "var(--lr-font-size-16)",
+            ]}
+          >
+            {getAddressShortcut(walletAddress)}
+          </Text>
+          <DisconnectWallet />
+        </Flex>
+        :
+        <ConnectWallet />}
+    </Flex>
+  )
+}
+
+const BurgerNavbar = ({
+  walletAddress
+}: {
+  walletAddress: string | undefined
+}) => {
+
+  return (
+    <LRDrawer
+      drawerTitle={
+        <LRLogo heigth="15px" />
+      }
+    >
+
+      <Stack>
+
+        <Flex h="60px" borderBottom="1px solid var(--lr-border-color-alpha-100)" align="center">
+          <Link href="/collections">
+            <Text
+              mr="2rem"
+              cursor="pointer"
+              fontSize="var(--lr-font-size-16)"
+            >
+              Collections
+            </Text>
+          </Link>
+        </Flex>
+
+        {walletAddress &&
+          <Flex h="60px" borderBottom="1px solid var(--lr-border-color-alpha-100)" align="center">
+            <Text mr="1rem" color="var(--lr-color-accent-green)" fontSize="var(--lr-font-size-16)">
+              {getAddressShortcut(walletAddress)}
+            </Text>
+          </Flex>
+        }
+
+        <Flex h="60px" align="center">
+          {walletAddress ? <DisconnectWallet /> : <ConnectWallet />}
+        </Flex>
+
+
+      </Stack>
+
+    </LRDrawer>
+  )
+}
+
 export default function Navbar() {
 
   const { walletAddress } = useContext(WalletProviderContext) as WalletProviderInterface
+
+  const [activateburgerNavbar, setActivateburgerNavbar] = useState(false)
+
+  useEffect(() => {
+
+    if (window.innerWidth < 480) {
+      setActivateburgerNavbar(true);
+    }
+
+    function handleResize() {
+      if (window.innerWidth < 480) {
+        setActivateburgerNavbar(true);
+      }
+      else if (window.innerWidth > 480) {
+        setActivateburgerNavbar(false);
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
 
   return (
     <Flex
@@ -21,24 +136,15 @@ export default function Navbar() {
       justify="space-between"
     >
 
-      <Box>
-        <LRLogo width="130px" heigth="5rem" />
-      </Box>
+      <LRLogo />
 
-      <Flex align="center">
-        <Link href="/collections">
-          <Text mr="2rem" cursor="pointer">
-            Collections
-          </Text>
-        </Link>
-        {walletAddress ?
-          <Flex align="center">
-            <Text mr="1rem" color="var(--lr-color-accent-green)">{getAddressShortcut(walletAddress)}</Text>
-            <DisconnectWallet />
-          </Flex>
+      {
+        activateburgerNavbar ?
+          <BurgerNavbar walletAddress={walletAddress} />
           :
-          <ConnectWallet />}
-      </Flex>
+          <NormalNavbar walletAddress={walletAddress} />
+      }
+
 
     </Flex>
   )
