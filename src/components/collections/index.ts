6dc46b393e5ea6a39d38150c750bf7a9c27af4d9
Oz4 <aowss18@gmail.com>
2022-07-25 12:03:55 +0300

    rename

--- src/components/collections/index.ts
+++ src/components/collections/index.ts
@@ -1,5 +1,6 @@
 import CollectionsListItem from "./CollectionsListItem"
 import CollectionsList from "./CollectionsList"
 import Header from "./Header"
-import CollectionListItemPhone from "./CollectionListItemPhone"
-export { CollectionsListItem, CollectionsList, Header, CollectionListItemPhone }
+import CollectionsListItemPhone from "./CollectionsListItemPhone"
+
+export { CollectionsListItem, CollectionsList, Header, CollectionsListItemPhone }
