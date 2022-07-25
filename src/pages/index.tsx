1cacda83cfd82fa64640e08dcf812f43301046aa
Oz4 <aowss18@gmail.com>
2022-07-25 18:19:03 +0300

    rename

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { Container, Flex } from '@chakra-ui/react'
-import { Meta } from 'components/common'
+import { MetaTags } from 'components/common'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
     <Container maxW="1440px" h="calc(100vh - 80px)">
 
-      <Meta
+      <MetaTags
         title={`LooksRare`}
         description="LooksRare is a next generation NFT market. Buy NFTs, sell NFTs… or just HODL: Collectors, traders, and creators alike earn passive income! 👀💎"
         url={`/`}
